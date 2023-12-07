1e4c6ebf5d948ca3f36995fb783077efeff4a3bf
qurriahSam <mugirethe1@gmail.com>
2023-12-07 20:42:12 +0300

    feat: get and post routes for items

---
+++ src/modules/items/itemsRouter.ts
@@ -0,0 +1,11 @@
+import { Router, urlencoded } from 'express';
+import { getItems, postItems } from './itemsController';
+
+const urlencodedParser = urlencoded({ extended: true });
+
+const itemsRouter = Router();
+
+itemsRouter.get('/', getItems);
+itemsRouter.post('/', urlencodedParser, postItems);
+
+export default itemsRouter;
