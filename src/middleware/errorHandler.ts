bbdb4fa5af54b95d0155f5e8260af55b6f274add
qurriahSam <mugirethe1@gmail.com>
2025-01-17 22:09:55 +0300

    style: remove unused variable

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,7 +1,7 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from "express";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (err: Error, req: Request, res: Response) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
 
   res.status(statusCode).json({ message: err.message });
